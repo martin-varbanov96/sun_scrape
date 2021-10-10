1fb10f7eeaf2e9dc11679ccf672fc7263a85e3c5
Martin Varbanov <martin_varbanov96@abv.bg>
2021-10-10 12:28:21 +0300

    bq basic

--- bq_manager/insert_bq.py
+++ bq_manager/insert_bq.py
@@ -3,7 +3,7 @@ from google.cloud import bigquery
 # Construct a BigQuery client object.
 client = bigquery.Client()
 
-table_id = "scrape-327418.sun_scrape_sun_data_set_id.bar"
+table_id = "static-emblem-327016.sun_scrape_sun_data_set_id.bar"
 
 rows_to_insert = [
     {u"title": u"title", "section": u"football", u"article_link": u"https://www.thesun.co.uk/sport/16267574/emre-tezgel-stoke-city-harry-kane-jude-bellingham/"},
--- sun_scrape_manager/sun_scrape_manager/spiders/football.py
+++ sun_scrape_manager/sun_scrape_manager/spiders/football.py
@@ -1,10 +1,28 @@
 import scrapy
+from sun_scrape_manager.items import SunScrapeManagerItem
 
 
 class FootballSpider(scrapy.Spider):
     name = 'football'
     allowed_domains = ['https://www.thesun.co.uk/sport/football/']
-    start_urls = ['http://https://www.thesun.co.uk/sport/football//']
+    start_urls = ['https://www.thesun.co.uk/sport/football//']
+
+    def __init__(self,):
+        self.section = "football"
 
     def parse(self, response):
-        pass
+
+        article_titles = response.xpath(
+            "//span[@class='rail__item-text']/h3/text()"
+            ).extract()
+ 
+        print("*" * 500)
+        print(article_titles)
+        print()
+
+        for article_title in article_titles:
+            yield SunScrapeManagerItem(news_link=response.url,
+                                            section=self.section,
+                                            title=article_title,
+                                            )        
+ 
